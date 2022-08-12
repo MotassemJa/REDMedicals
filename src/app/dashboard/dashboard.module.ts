9d653c8b4eeef1b56c417621b6367fdd9f28d903
Motassem Ja <M.j.92@live.com>
2022-08-12 17:53:01 +0200

    fix: fix first bug - 1. Aufgabe

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { SearchModule } from '@red-probeaufgabe/search';
+import { AbstractSearchFacadeService, SearchFacadeService, SearchModule } from '@red-probeaufgabe/search';
 import { UiModule } from '@red-probeaufgabe/ui';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [CommonModule, UiModule, SearchModule, DashboardRoutingModule],
   exports: [DashboardComponent],
+  providers: [
+    // When AbstractSearchFacadeService is needed, use SearchFacadeService instead
+    {
+      provide: AbstractSearchFacadeService,
+      useClass: SearchFacadeService
+    }
+  ]
 })
 export class DashboardModule {}
